542676ccd0ee133b37e533aae1cf4e082e5a966a
Petr <petr.hedgehog@gmail.com>
2022-11-21 21:09:30 +0100

    Muj prvni prispevek na Git

--- prvni_projekt.py
+++ prvni_projekt.py
@@ -1,2 +1,9 @@
-"TEXT ANALYZATOR"
+"""TEXT ANALYZATOR"""
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
 
+author: Petr Jezek
+email: petr.hedgehog@gmail.com
+discord: Beloba#2442
+"""
+import ...
